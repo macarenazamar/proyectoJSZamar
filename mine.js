4768de265ad1a4c236d02d42b66553e8fb5474ee
macarena-zamar <mmacarenazamar@gmail.com>
2022-04-19 19:37:32 -0300

    En rama master inicio de arrays y objetos, pendiente consultas realizadas

--- mine.js
+++ mine.js
@@ -5,9 +5,26 @@ class Producto{
         this.stock = stock;
     }
 }
+const producto1 = new Producto("Bombas", 67500, 10);
+const producto2 = new Producto("Motores", 30500, 15);
+const producto3 = new Producto("Guarniciones", 1200, 50);
+const producto4 = new Producto("Direciones Hidrostáticas", 52600, 10);
+const producto5 = new Producto("Electroválvulas", 2300, 50);
+const producto6 = new Producto("Radiadores", 10500, 5);
 
-
-
+class Servicio{
+    constructor(nombre, precio, stock){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
+const servicio1 = new Servicio("Centrales Hiráulicas", 167500, 10);
+const servicio2 = new Servicio("Ascensores Hirdráulicos", 130500, 15);
+const servicio3 = new Servicio("Máquinas Agrícolas", 111200, 50);
+const servicio4 = new Servicio("Grúas y Montacargas", 152600, 10);
+const servicio5 = new Servicio("Cilindros Hidráulicos", 112300, 50);
+const servicio6 = new Servicio("Trasnsmiciones Hidrostáticas", 210500, 5);
 
 
 
