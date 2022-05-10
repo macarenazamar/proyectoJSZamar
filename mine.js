70cc14e88a2e4df658bc69316ecc24bf849318e9
macarena-zamar <mmacarenazamar@gmail.com>
2022-05-10 10:36:29 -0300

    En rama master: Formas de pago segunda entrega

--- mine.js
+++ mine.js
@@ -1,79 +1,84 @@
-class Producto{
-    constructor(nombre, precio, stock){
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-    }
+let cuotas = [
+    {
+        nombre: "Contado",
+        cantidad: 1,
+    },
+    {
+        nombre: "3 Cuotas",
+        cantidad: 3,
+    },
+    {
+        nombre: "6 Cuotas",
+        cantidad: 6,
+    },
+    {
+        nombre: "12 Cuotas",
+        cantidad: 12,
+    },
+];
+
+localStorage.clear();
+sessionStorage.clear();
+
+//Creo función cuotas
+function agregarCuotaAHtml (cuota){
+    let ul = document.createElement("ul");
+
+    let li1 = document.createElement("li");
+    li1.innerText = "Usted selecionó " + cuota.nombre;
+
+    let li2 = document.createElement("li");
+    li2.innerText = "Son " + cuota.cantidad + " cuotas sin interes";
+
+    ul.append(li1, li2);
+
+    contenedor.append(ul);
 }
-const producto1 = new Producto("Bombas", 67500, 10);
-const producto2 = new Producto("Motores", 30500, 15);
-const producto3 = new Producto("Guarniciones", 1200, 50);
-const producto4 = new Producto("Direciones Hidrostáticas", 52600, 10);
-const producto5 = new Producto("Electroválvulas", 2300, 50);
-const producto6 = new Producto("Radiadores", 10500, 5);
+//Seleccionamos el contenedor
 
-class Servicio{
-    constructor(nombre, precio, stock){
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-    }
-}
-const servicio1 = new Servicio("Centrales Hiráulicas", 167500, 10);
-const servicio2 = new Servicio("Ascensores Hirdráulicos", 130500, 15);
-const servicio3 = new Servicio("Máquinas Agrícolas", 111200, 50);
-const servicio4 = new Servicio("Grúas y Montacargas", 152600, 10);
-const servicio5 = new Servicio("Cilindros Hidráulicos", 112300, 50);
-const servicio6 = new Servicio("Trasnsmiciones Hidrostáticas", 210500, 5);
+let contenedor = document.getElementById("contenedor"); 
 
+//Seleccionamos el select
+let select = document.getElementById("select-cuota");
 
+//Creamos la opción nula
+let optionNula = document.createElement("option");
+optionNula.value = "";
+optionNula.innerText = "Seleccionar forma de pago";
+select.append(optionNula);
 
+//Recorremos las cuotas
+cuotas.forEach((cuota) =>{
+    //Creamos la opción del select
+    let option = document.createElement("option");
+    option.value = cuota.cantidad;
+    option.innerText = cuota.nombre;
+    
+    //Agregamos la opción al select
+    select.append(option); 
+});
 
+//Seleccionamos botón
+let boton = document.getElementById("boton-1");
+boton.addEventListener("click", () => {
+    const valueSelect = select.value;
 
-let precio = 0;
-
-function consultarPrecio() {
-
-    let volverAConsultar = false;
-
-    do {
-
-        let oferta = '';
-
-        do {
-            oferta = prompt("¿Queres conocer el precio de nuestros PRODUCTOS o SERVICIOS?").toLocaleUpperCase();
-
-            if (oferta !== "PRODUCTO" && oferta !== "SERVICIO"){
-                alert("Ingresa PRODUCTO o SERVICIO");
-            }
-            
-        } while ((oferta !== "PRODUCTO") && (oferta !== "SERVICIO"));
-
-        switch (oferta){
-            case "PRODUCTO":
-                precio = 4608;
-                break;
-            case "SERVICIO":
-                precio = 5732;
-                break;
-        }
-
-        alert ("El precio es de " + precio + " dólares");
-        volverAConsultar = confirm("¿Consultar otro valor?");
-    } while (volverAConsultar);
-
-}
-
-function cambioArg (precio){
-    let confirmation = confirm ("¿Convertimos a pesos?");
-
-    if (confirmation && precio){
-        alert("El precio es de " + (precio * 200) + " pesos");
-    } else{
-        alert("El precio es de " + precio + " dólares");
+    if(valueSelect !== ""){
+        const cuotaEncontrada = cuotas.find( (cuota) => {
+            return cuota.cantidad == valueSelect;
+        } );
+        agregarCuotaAHtml(cuotaEncontrada);
+        
     }
 
-}
+    
+    
+})
 
-consultarPrecio();
-cambioArg(precio);
+//Cómo hago para guardar en sessionStorage solo cuando el usario apreta al botón de pagar?
+//Dónde lo pongo? dentro de los corchetes del botón? abajo de todo, dentro del contenedor? eso es otra cosa que no sé dónde va
+// Intenté ahcer algo de esto:
+//Almacenamos en sessionStorage forma de pago seleccionada
+//sessionStorage.setItem("forma_de_pago" -ES LA KEY-, agregarCuotaAHtml -ES EL VALOR-); --> está mal porque me tira error
+//const fomraDePago = JSON.parse(sessionStorage.getItem(QUÉ VA ACÁ?))
+//No entiendo :´(
